3182a66b7a7f2d4e8902d6feff4a4b4b4686794e
Flaming <yeming945@qq.com>
2020-07-03 13:42:33 +0800

    12.3.3 创建设置类

--- 12/alien_invasion.py
+++ 12/alien_invasion.py
@@ -1,10 +1,13 @@
 import sys
 import pygame
+from settings import Settings
 
 
 def run_game():
     pygame.init()  # 初始化游戏并创建一个屏幕对象
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     bg_color = (230, 230, 230)  # 设置背景色
@@ -15,7 +18,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        screen.fill(bg_color)  # 每次循环时都重绘屏幕
+        screen.fill(ai_settings.bg_color)  # 每次循环时都重绘屏幕
 
         pygame.display.flip()  # 让最近绘制的屏幕可见
 
