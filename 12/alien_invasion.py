0f76da42c13fe7577ad4c7bc609414e2572458e9
Flaming <yeming945@qq.com>
2020-07-03 14:30:10 +0800

    12.4.2 在屏幕上绘制飞船

--- 12/alien_invasion.py
+++ 12/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -12,6 +13,8 @@ def run_game():
 
     bg_color = (230, 230, 230)  # 设置背景色
 
+    ship = Ship(screen)  # 创建一艘飞船
+
     while True:  # 开始游戏主循环
         # 监视键盘和鼠标事件
         for event in pygame.event.get():
@@ -19,6 +22,7 @@ def run_game():
                 sys.exit()
 
         screen.fill(ai_settings.bg_color)  # 每次循环时都重绘屏幕
+        ship.blitem()  # 放置飞船
 
         pygame.display.flip()  # 让最近绘制的屏幕可见
 
